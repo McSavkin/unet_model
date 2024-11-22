16956b736c63d8c8314698be59f4e10806bfbf63
Danil-Oo <danilsints@gmail.com>
2024-11-22 20:53:49 +0700

    mmm

--- unet_page.py
+++ unet_page.py
@@ -1,5 +1,5 @@
 import os
-from models.unet import UNet  # класс модели Unet
+# from models.unet import UNet  # класс модели Unet
 import streamlit as st
 import torch
 from torchvision import transforms
@@ -8,9 +8,115 @@ import requests
 from io import BytesIO
 import numpy as np
 import sys
+import torch.nn as nn
+import torch.nn.functional as F
 # sys.path.append('./models')  # Убедитесь, что Streamlit видит папку models
 
+
+class UNet(nn.Module):
+    def __init__(self, n_class):
+        super(UNet, self).__init__()
+
+        # Encoder
+        self.e11 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.e12 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.e21 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.e22 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.e31 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.e32 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.e41 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.e42 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.e51 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
+        self.e52 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1)
+
+        # Decoder
+        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        # Ожидается 1024 канала после конкатенации
+        self.d11 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
+        self.d12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+
+        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        # Ожидается 512 канала после конкатенации
+        self.d21 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
+        self.d22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+
+        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        # Ожидается 256 канала после конкатенации
+        self.d31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.d32 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+
+        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        # Ожидается 128 канала после конкатенации
+        self.d41 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.d42 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+
+        # Output layer
+        self.outconv = nn.Conv2d(64, n_class, kernel_size=1)
+
+        # Сигмоида для бинарной сегментации
+        self.sigmoid = nn.Sigmoid()  # Для бинарной сегментации
+
+    def forward(self, x):
+        # Encoder
+        xe11 = F.relu(self.e11(x))
+        xe12 = F.relu(self.e12(xe11))
+        xp1 = self.pool1(xe12)
+
+        xe21 = F.relu(self.e21(xp1))
+        xe22 = F.relu(self.e22(xe21))
+        xp2 = self.pool2(xe22)
+
+        xe31 = F.relu(self.e31(xp2))
+        xe32 = F.relu(self.e32(xe31))
+        xp3 = self.pool3(xe32)
+
+        xe41 = F.relu(self.e41(xp3))
+        xe42 = F.relu(self.e42(xe41))
+        xp4 = self.pool4(xe42)
+
+        xe51 = F.relu(self.e51(xp4))
+        xe52 = F.relu(self.e52(xe51))
+
+        # Decoder
+        xu1 = self.upconv1(xe52)
+        # Concatenate corresponding encoder feature maps
+        xu1 = torch.cat([xu1, xe42], dim=1)
+        xd11 = F.relu(self.d11(xu1))
+        xd12 = F.relu(self.d12(xd11))
+
+        xu2 = self.upconv2(xd12)
+        # Concatenate corresponding encoder feature maps
+        xu2 = torch.cat([xu2, xe32], dim=1)
+        xd21 = F.relu(self.d21(xu2))
+        xd22 = F.relu(self.d22(xd21))
+
+        xu3 = self.upconv3(xd22)
+        # Concatenate corresponding encoder feature maps
+        xu3 = torch.cat([xu3, xe22], dim=1)
+        xd31 = F.relu(self.d31(xu3))
+        xd32 = F.relu(self.d32(xd31))
+
+        xu4 = self.upconv4(xd32)
+        # Concatenate corresponding encoder feature maps
+        xu4 = torch.cat([xu4, xe12], dim=1)
+        xd41 = F.relu(self.d41(xu4))
+        xd42 = F.relu(self.d42(xd41))
+
+        # Output layer
+        out = self.outconv(xd42)
+        out = self.sigmoid(out)  # Применяем сигмоиду для бинарной сегментации
+        return out
 # Глобальная переменная для модели
+
+
 model_unet = None
 
 
