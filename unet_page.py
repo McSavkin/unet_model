88e480c4cdaed156f752ad75c0b2cb173d92958d
Taralina <titova.a.0105@gmail.com>
2024-11-22 09:25:39 +0300

    10 try

--- unet_page.py
+++ unet_page.py
@@ -8,44 +8,53 @@ import numpy as np
 from unet import UNet  # класс модели Unet
 from tensorboard import program
 import os
-import cv2
+
+# Глобальная переменная для модели
+model_unet = None
+
+def load_model():
+    global model_unet
+    if model_unet is None:
+        # Проверка на доступность GPU
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model_unet = UNet(n_class=1).to(device)
+        model_unet.load_state_dict(torch.load('models/model_weights.pth', map_location=device))
+        model_unet.eval()  # Устанавливаем модель в режим оценки
+    return model_unet
+
+# Функция для предсказания сегментации
+@st.cache_data
+def predict_image(model, image, device):
+    # Загружаем изображение
+    image = Image.open(image).convert("RGB")
+    
+    # Предобработка изображения (изменение размера и нормализация)
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),   # Размер изображения
+        transforms.ToTensor(),           # Преобразуем в тензор
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Нормализация
+    ])
+    
+    # Применяем преобразования
+    image_tensor = transform(image).unsqueeze(0).to(device)  # Добавляем размерность для батча (1, C, H, W)
+
+    # Получаем предсказание
+    model.eval()  # Переводим модель в режим оценки
+    with torch.no_grad():  # Без расчета градиентов
+        output = model(image_tensor)  # Предсказание модели
+    
+    # Применение сигмоида (для бинарной сегментации)
+    output = torch.sigmoid(output)  # Если выход модели был без активации, используем сигмоиду для нормализации
+    
+    # Преобразуем предсказания в 0 или 1 (для бинарной сегментации)
+    output = (output > 0.5).float()  # Используем порог 0.5 для бинаризации
+    
+    return output.squeeze(0)  # Убираем размерность батча
 
 def unet_segmentation():
-
-    # Загрузка модели Unet
+    # Загрузка модели один раз
+    model_unet = load_model()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_unet = UNet(n_class=1).to(device)
-    model_unet.load_state_dict(torch.load('models/model_weights.pth', map_location=device))
-    model_unet.eval()  # Устанавливаем модель в режим оценки
-
-    # Функция для получения предсказания
-    def predict_image(model, image, device):
-        # Загружаем изображение
-        image = Image.open(image).convert("RGB")
-    
-        # Предобработка изображения (например, изменение размера и нормализация)
-        transform = transforms.Compose([
-            transforms.Resize((256, 256)),   # Размер изображения
-            transforms.ToTensor(),           # Преобразуем в тензор
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Нормализация
-        ])
-    
-        # Применяем преобразования
-        image_tensor = transform(image).unsqueeze(0).to(device)  # Добавляем размерность для батча (1, C, H, W)
-
-        # Получаем предсказание
-        model.eval()  # Переводим модель в режим оценки
-        with torch.no_grad():  # Без расчета градиентов
-            output = model(image_tensor)  # Предсказание модели
-        
-        # Применение сигмоида (для бинарной сегментации)
-        output = torch.sigmoid(output)  # Если выход модели был без активации, используем сигмоиду для нормализации
-    
-        # Преобразуем предсказания в 0 или 1 (для бинарной сегментации)
-        output = (output > 0.5).float()  # Используем порог 0.5 для бинаризации
-    
-        return output.squeeze(0)  # Убираем размерность батча
-
 
     # Streamlit интерфейс
     st.title("Применение модели Unet к задаче сегментации")
@@ -59,13 +68,8 @@ def unet_segmentation():
 
         if uploaded_files is not None:
             for uploaded_file in uploaded_files:
-                # Сохранить изображение на диск
-                image_path = 'temp_image.jpg'
-                with open(image_path, "wb") as f:
-                    f.write(uploaded_file.getbuffer())
-            
                 # Получаем предсказание
-                output_mask = predict_image(model_unet, image_path, device)
+                output_mask = predict_image(model_unet, uploaded_file, device)
                 
                 # Преобразуем маску в правильный формат для отображения
                 output_mask = output_mask.cpu().numpy() * 255  # Преобразуем в диапазон [0, 255] для отображения
@@ -93,12 +97,8 @@ def unet_segmentation():
                     response = requests.get(image_url)
                     image = Image.open(BytesIO(response.content)).convert("RGB")
                 
-                    # Сохраняем изображение для предсказания
-                    image_path = 'temp_image_from_url.jpg'
-                    image.save(image_path)
-                
                     # Получаем предсказание
-                    output_mask = predict_image(model_unet, image_path, device)
+                    output_mask = predict_image(model_unet, image, device)
                 
                     # Преобразуем маску в правильный формат для отображения
                     output_mask = output_mask.cpu().numpy() * 255  # Преобразуем в диапазон [0, 255] для отображения
@@ -118,15 +118,16 @@ def unet_segmentation():
 
                 except Exception as e:
                     st.error(f"Error loading image from URL: {e}")
+    
     # Функция для запуска TensorBoard
     def run_tensorboard(logdir):
-    # Запуск TensorBoard в фоновом процессе
+        # Запуск TensorBoard в фоновом процессе
         tb = program.TensorBoard()
         tb.configure(argv=[None, '--logdir', logdir])
         url = tb.launch()
         return url
 
-    # Создание интерфейса Streamlit
+    # Создание интерфейса Streamlit для TensorBoard
     st.title("Интеграция TensorBoard с Streamlit")
 
     # Введите директорию с логами
